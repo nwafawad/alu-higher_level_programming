97879389246e01e6fde3852b81c31cccbb9e0b5e
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-03 05:01:29 -0700

    concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School"
-#concat both str
-print(str1, str2, "!")
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")
+
