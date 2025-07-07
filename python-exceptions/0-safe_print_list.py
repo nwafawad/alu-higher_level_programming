110a9ac6a579fdf000dcbbb5f9fac92c38670427
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-07 15:43:13 -0700

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             count += 1
+
         except IndexError:
             break
-    print() 
+    print()
     return count
