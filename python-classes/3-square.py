7182978f4a7c5b996a3a2d9f8c181904fb24dbca
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-11 14:45:42 -0700

    Class

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square module with area method"""
+
+class Square:
+    """Defines a square with size validation"""
+
+    def __init__(self, size=0):
+        """Initialize the square with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square"""
+        return self.__size ** 2
