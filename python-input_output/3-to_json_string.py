329ff93356b7f4c6dd1b3f341c773e63d0baa830
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-17 16:04:00 -0700

    pythos <> Json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import json
-
-
 """
 this module converts python object 
 to Json string.
 """
+
+
 def to_json_string(my_obj):
     """Return the JSON representation of a Python object"""
     return json.dumps(my_obj)
