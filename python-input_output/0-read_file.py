e9a6abd426aead37be0d45de516fc43b63175e3f
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-17 07:02:20 -0700

    read myfile0.txt

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Module for reading and printing the content of a file"""
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents"""
     with open(filename, 'r', encoding='utf-8') as f:
