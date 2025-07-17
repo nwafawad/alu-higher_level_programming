0f2b1e4fb45fa7598083c56dcbff8b0ddedd9d37
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-17 06:51:54 -0700

    read content of my file0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its contents"""
+    #Reads a UTF-8 text file and prints its contents
     with open(filename, 'r', encoding='utf-8') as f:
+        #print the content of the dile using .read() function
         print(f.read(), end="")
