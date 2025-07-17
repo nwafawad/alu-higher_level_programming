e6a514b963a7ca5d3ff13545b6e45eaafa0b84c5
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-17 07:10:01 -0700

    red file0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Module for reading and printing the content of a file"""
+""" Module for reading and printing the content of a file
+This module contains a single function called 'read_file' which opens a file inread mode and prints its entire content to standard output.
+"""
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents"""
     with open(filename, 'r', encoding='utf-8') as f:
-       """ print the content of the dile using .read() function"""
+        """ print the content of the dile using .read() function"""
         print(f.read(), end="")
