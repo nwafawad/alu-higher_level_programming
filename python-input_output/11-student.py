54bd9f1b15eebc4bfb538dc2a15fcc792036b369
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-18 14:45:28 -0700

    update

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Student class for JSON serialization
-and deserialization."""
+"""
+Student class for JSON serialization
+and deserialization.
+"""
 
 
 class Student:
@@ -9,13 +11,13 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         if isinstance(attrs, list):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+            return {key: getattr(self, key) for key in
+                    attrs if hasattr(self, key)}
         return self.__dict__
 
     def reload_from_json(self, json):
         for key, value in json.items():
             setattr(self, key, value)
-
