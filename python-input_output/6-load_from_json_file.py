eab0f047a2c683561176d52e31143b5bc3d34aaa
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-18 11:27:00 -0700

    updates

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,6 +7,7 @@ and returns the corresponding Python object.
 
 import json
 
+
 def load_from_json_file(filename):
     """Creates a Python object from a JSON file"""
     with open(filename, 'r', encoding='utf-8') as f:
