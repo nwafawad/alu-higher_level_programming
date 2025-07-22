c52fc58a1655498d79eabfe9120e67f50cfa144f
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-22 12:22:18 -0700

    update

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -38,9 +38,9 @@ def height(self, value):
     self.__height = value
 
 def area(self):
-        return self.width * self.height
+    return self.width * self.height
 
 def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        return 2 * (self.width + self.height)
+    if self.width == 0 or self.height == 0:
+        return 0
+    return 2 * (self.width + self.height)
