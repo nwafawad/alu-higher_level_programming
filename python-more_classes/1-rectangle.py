a6920447991f52b3ccbb0d2add17a30090c4fb4b
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-22 12:23:35 -0700

    update

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
-        if value < 0 :
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -36,7 +36,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
-        if value < 0 :
+        if value < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
