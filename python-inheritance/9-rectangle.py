cee227cfe0f0a49c11f9176c0e489051b6360bbd
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-25 14:27:19 -0700

    update

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+contains the class Basegeometry and subclass
+rectangle
+"""
+
+
+class BaseGeometry:
+    """
+    class with public instance methods
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """representation of a rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
