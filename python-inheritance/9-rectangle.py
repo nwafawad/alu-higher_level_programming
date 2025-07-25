d29bcb84dded6989161b0a64585df3417def24af
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-25 14:30:38 -0700

    update

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,11 +11,13 @@ class BaseGeometry:
     """
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """representation of a rectangle"""
