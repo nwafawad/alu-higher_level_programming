57db64107ff0e7e29c911f07cbe9e39eeeb16bdf
nwafawad49@gmail.com <nwafawad49@gmail.com>
2025-07-25 08:19:09 -0700

    class inherted

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this module defines an inherited class-checking
+function.
+"""
+
+def inherits_from(obj, a_class):
+    """checks if an object is an inherited instance
+    of a class """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
